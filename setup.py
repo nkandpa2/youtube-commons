f18d700df4ccad22a895ab619187f2bd8409a5e5
Nikhil Kandpal <nkandpa2@gmail.com>
2024-03-04 08:50:16 -0500

    fix path in setup.py

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setup(
     install_requires=requirements,
     entry_points={
         'console_scripts': [
-            'cc-videos = yt_commons.scripts.cc_videos:main',
+            'cc-videos = youtube_commons.scripts.cc_videos:main',
         ],
     },
     # Add classifiers to specify the audience and maturity of your package
